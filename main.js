006fb58f9e4d1eeef23018b84912aa832c27e972
Guyon David <davidguyon49@gmail.com>
2013-03-30 16:58:50 +0000

    Add: show/hide projects list

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+/*
+ * Global variables
+ */
+var projectsListExpanded = false;
+
 $(function(){
+	/* Hide all project boxes */
+	$("#projectsList div").hide();
+
 	handleWhereBox();
 	handleWhoBox();
 	handleWebsiteBox();
@@ -74,12 +82,27 @@ function handleBlogBox() {
 
 function handleProjectsBox() {
 	$("#projects").click(function() {
-		hideWhoBox(1000);
-		hideWebsiteBox(1000);
-		setTimeout(function() {
+		if(!projectsListExpanded) {
 			hideWhereBox(1000);
 			hideBlogBox(1000);
-		}, 600);
+			hideWhoBox(1000);
+			hideWebsiteBox(1000);
+			setTimeout(function() {
+				showProjectsList(800);
+				$("#projects p").html("Click to go back");
+				projectsListExpanded = true;
+			}, 600);
+		} else {
+			hideProjectsList(400);
+			setTimeout(function() {
+				showWhoBox(1000);
+				showWebsiteBox(1000);
+					showWhereBox(1000);
+					showBlogBox(1000);
+					$("#projects p").html("Click to see all projects I did or that I'm currently doing");
+					projectsListExpanded = false;
+			}, 800);
+		}
 	});
 }
 
@@ -97,4 +120,47 @@ function hideWhereBox(duration) {
 
 function hideBlogBox(duration) {
 	$("#blog").hide("blind", duration);
+}
+
+function showWhoBox(duration) {
+	$("#who").show("blind", duration);
+}
+
+function showWebsiteBox(duration) {
+	$("#website").show("blind", duration);
+}
+
+function showWhereBox(duration) {
+	$("#where").show("blind", duration);
+}
+
+function showBlogBox(duration) {
+	$("#blog").show("blind", duration);
+}
+
+function showProjectsList(duration) {
+	$("#projectsList div").each(function(index, element) {
+		var that = $(this);
+		setTimeout(function() {
+			that.show();
+			that.animate({
+				opacity: 1
+			}, duration);
+		}, 400 * index);
+	});
+}
+
+function hideProjectsList(duration) {
+	$($("#projectsList div").get().reverse()).each(function(index, element) {
+		var that = $(this);
+		setTimeout(function() {
+			that.animate({
+				opacity: 0
+			}, duration, function() {
+				setTimeout(function() {
+					that.hide();
+				}, 150);
+			});
+		}, 400 * index);
+	});
 }
